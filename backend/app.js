becde6ec95c75e6371e94a8037e46bc21de13193
Clara hlt <clarahltc@gmail.com>
2022-05-29 13:38:20 +0200

    [modifications] server file

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,10 @@ const bodyParser = require('body-parser');
 //Permet d'importer les routes
 const userRoutes = require('./routes/user');
 
-const server = express();
+const app = express();
 
 //Middleware CORS
-server.use((req, res, next) => {
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
@@ -17,19 +17,17 @@ server.use((req, res, next) => {
   });
 
 //Transforme le corps de la requête en objet JS
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //Permet d'accéder aux Users Routes
-server.use('/api/auth/', userRoutes);
-server.use('/images', express.static(path.join(__dirname, 'images')));
-server.use('/api/users/', userRoutes);
+app.use('/api/auth/', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/users/', userRoutes);
 
 //Permet d'accéder aux Posts Routes
-server.use('/messages/create', userRoutes);
-server.use('/messages/', userRoutes);
+app.use('/messages/create', userRoutes);
+app.use('/messages/', userRoutes);
 
 
-server.listen(3000, function(){
-    console.log('Server en écoute')
-});
+module.exports = app;
